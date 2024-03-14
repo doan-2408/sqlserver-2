2e4463e5a287b31bd109b904d85fcc3bc486ef8f
doan-2408 <163369813+doan-2408@users.noreply.github.com>
2024-03-14 18:07:31 +0700

    add file

---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+﻿use QLDA
+--lấy tất cả thông tin nhân viên nữ
+--b1 xác định bảng cần dùng 
+--xác định dữ liệu cần lấy 
+--xác định điều kiện
+select * from NHANVIEN where phai like N'nữ'
+--Lấy ra thông tin phòng ban có số thành viên >2
+select*from nhanvien;
+select PHG, count(MANV) as'số lượng thành viên' from NHANVIEN
+group by PHG
+having count (MANV) >2;
+--Lấy thông tin mã nhân viên, tên nhân viên, mã phòng ban, tên phòng ban
+select MANV, TENNV, PHG, TENPHG from NHANVIEN 
+join PHONGBAN on NHANVIEN.PHG =PHONGBAN.MAPHG
